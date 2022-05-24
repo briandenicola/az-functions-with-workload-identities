a45d1a2fea5bfdaaac96e6fa6281f2f54ca8b672
briandenicola <brian@briandenicola.com>
2022-05-24 07:34:23 -0500

    Refactored

--- src/eventprocessor/commandProcessing.cs
+++ src/eventprocessor/commandProcessing.cs
@@ -1,15 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json; 
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-
-using Azure.Core;
-using Azure.Identity;
-
 using Azure.Messaging.EventHubs;
 using Microsoft.Extensions.Logging;
 
@@ -18,19 +7,14 @@ namespace Eventing
     public static class CommandProcessing
     {
         [FunctionName("CommandProcessing")]
-        public static void Run(
-        
-            [EventHubTrigger(
-                "requests",
-                ConsumerGroup =  "functions-client",
-                Connection = "EVENTHUB_CONNECTION")] string[] events,
+        public static void Run(    
+            [EventHubTrigger( "requests", ConsumerGroup =  "functions-client", Connection = "EVENTHUB_CONNECTION")]  EventData[] events,
             ILogger log)
         {
-            foreach ( var message in events )
+            foreach (EventData eventData in events)
             {
-                log.LogInformation($"C# Event Hub trigger function processed a message: {message}");
+                log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
             }
-
         }
     }
 }

ae732dc6f4500735f0415dd05fca1d478c1398b7
briandenicola <brian@briandenicola.com>
2022-05-24 10:25:10 -0500

    Removed Connection String Requirement

---
+++ src/client/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using System.Text;
+global using System.CommandLine;
+global using Azure.Messaging.EventHubs;
+global using Azure.Messaging.EventHubs.Producer;
+global using Azure.Identity;
--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,31 +1,42 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Producer;
+﻿const string eventHubName = "requests";
+
+var namespaceOption = new Option<string>(
+        new [] {"--event-hub-namespace", "-n"},
+        description: "The Event Hub Namespace name to push sample events to");
 
 
-//Fix me
-//Example: https://github.com/briandenicola/todo-app-azure-sql/blob/acdac15dc6fb441c2b019141467b657f40a78a11/src/Program.cs
-const string connectionString = "";
-const string eventHubName = "requests";
- 
-EventHubProducerClient producerClient = new EventHubProducerClient(connectionString, eventHubName);
-
-using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
-
-for (int i = 1; i <= 5; i++)
+var rootCommand = new RootCommand
 {
-    if (! eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}"))))
-    {
-        throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
-    }
-}
+    namespaceOption
+};
 
-try {
-    await producerClient.SendAsync(eventBatch);
-    Console.WriteLine($"A batch of 5 events has been published.");
-}
-finally {
-    await producerClient.DisposeAsync();
-}
+
+rootCommand.Description = "A client app to publish sample events to Azure Event Hub";
+rootCommand.SetHandler( async (string eventhubNameSpace) =>
+{  
+    var credential = new ChainedTokenCredential(new AzureCliCredential());
+    var eventHubNameSpaceFQDN = $"{eventhubNameSpace}.servicebus.windows.net";
+    var producerClient = new EventHubProducerClient(eventHubNameSpaceFQDN, eventHubName, credential);
+    
+    using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+    for (int i = 1; i <= 5; i++)
+    {
+        if (! eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}"))))
+        {
+            throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+        }
+    }
+
+    try {
+        await producerClient.SendAsync(eventBatch);
+        Console.WriteLine($"A batch of 5 events has been published.");
+    }
+    finally {
+        await producerClient.DisposeAsync();
+    }
+}, namespaceOption);
+
+return rootCommand.InvokeAsync(args).Result;
+
+

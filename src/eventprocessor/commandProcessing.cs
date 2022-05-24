0886e40f56e57d59f0f0834069c533783d7fc0c1
briandenicola <brian@briandenicola.com>
2022-05-24 15:23:39 -0500

    Dapr Extension to PostgreSQL

--- src/eventprocessor/commandProcessing.cs
+++ src/eventprocessor/commandProcessing.cs
@@ -1,19 +1,23 @@
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Azure.Messaging.EventHubs;
 using Microsoft.Extensions.Logging;
+using Dapr.AzureFunctions.Extension;
 
 namespace Eventing
 {
     public static class CommandProcessing
     {
         [FunctionName("CommandProcessing")]
-        public static void Run(    
+        public static async Task Run(    
             [EventHubTrigger( "requests", ConsumerGroup =  "functions-client", Connection = "EVENTHUB_CONNECTION")]  EventData[] events,
+            [DaprState("statestore", Key = "sample")] IAsyncCollector<string> state,
             ILogger log)
         {
             foreach (EventData eventData in events)
             {
                 log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                await state.AddAsync(eventData.EventBody.ToString());
             }
         }
     }

ce2dd3a2ae18562bd3fb0d06f5e5c978e0983cdd
briandenicola <brian@briandenicola.com>
2022-09-26 14:01:02 -0500

    Provider Version bump

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     azurerm  = {
       source = "hashicorp/azurerm"
-      version = "3.3.0"
+      version = "3.24.0"
     }
     azapi = {
       source = "Azure/azapi"
-      version = "0.1.1"
+      version = "0.6.0"
     }
     helm = {
       source = "hashicorp/helm"
-      version = "2.5.1"
+      version = "2.6.0"
     }
   }
 }

3359832a806f1b547a9660b44333e702128ea994
briandenicola <brian@briandenicola.com>
2023-08-11 12:52:45 -0500

    Added access to Queues

--- infrastructure/roles.tf
+++ infrastructure/roles.tf
@@ -25,9 +25,29 @@ resource "azurerm_role_assignment" "blob" {
   skip_service_principal_aad_check = true  
 }
 
+resource "azurerm_role_assignment" "storage" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
+  skip_service_principal_aad_check = true  
+}
+
+resource "azurerm_role_assignment" "queue" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
+  skip_service_principal_aad_check = true  
+}
+
 resource "azurerm_role_assignment" "hub" {
   scope                = azurerm_eventhub_namespace.this.id
   role_definition_name = "Azure Event Hubs Data Owner"
   principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
   skip_service_principal_aad_check = true  
+}
+
+resource "azurerm_role_assignment" "client" {
+  scope                = azurerm_eventhub_namespace.this.id
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -36,3 +36,34 @@ resource "azurerm_private_endpoint" "storage_account" {
     private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_blob_core_windows_net.id]
   }
 }
+
+resource "azurerm_private_dns_zone" "privatelink_queue_core_windows_net" {
+  name                = "privatelink.queue.core.windows.net"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_queue_core_windows_net" {
+  name                  = "${azurerm_virtual_network.this.name}-link"
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink_queue_core_windows_net.name
+  resource_group_name   = azurerm_resource_group.this.name
+  virtual_network_id    = azurerm_virtual_network.this.id
+}
+
+resource "azurerm_private_endpoint" "storage_account_queue" {
+  name                = "${local.storage_account_name}-endpoint"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  subnet_id           = azurerm_subnet.private-endpoints.id
+
+  private_service_connection {
+    name                           = "${local.storage_account_name}-link"
+    private_connection_resource_id = azurerm_storage_account.this.id
+    subresource_names              = ["queue"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = azurerm_private_dns_zone.privatelink_queue_core_windows_net.name
+    private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_queue_core_windows_net.id]
+  }
+}
